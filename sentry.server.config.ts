3dbc6833bd2cdaf8d6144766f99acf248eedbb0f
Rahul Chhabria <rahul.chhabria@sentry.io>
2025-01-26 12:30:49 -0800

    moving dsn to .env

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://891119ef51d56b1a8c9193f32047d068@o4506312335294464.ingest.us.sentry.io/4508672160628736",
+  dsn: process.env.SENTRY_DSN,
   
   // Enable debug mode to see verbose logging
   debug: true,
