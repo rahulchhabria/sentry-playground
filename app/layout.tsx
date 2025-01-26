4744df3c540e34b4ec5777b9b975d0c2c270d2d7
Rahul Chhabria <rahul.chhabria@sentry.io>
2025-01-25 20:44:01 -0800

    set up vercel's speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Providers } from './providers';
 import { SentryToolbarProvider } from '../src/components/SentryToolbarProvider';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
 <SentryToolbarProvider>
 <Providers>{children}</Providers>
 </SentryToolbarProvider>
+<SpeedInsights />
       </body>
     </html>
   );
